610fab4a4ad94ee5129175ddab8deb897c1f3cb8
yuguan <yuguan>
2019-03-12 12:58:25 -0400

    1 [CHG] add local debug console demo project;

---
+++ AspNetCoreDebugConsoleDemo/DebugService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace AspNetCoreDebugConsoleDemo
+{
+    public class DebugService
+    {
+        private readonly ILogger _logger;
+        private readonly IConfiguration _configuration;
+
+        public DebugService(
+            ILogger<DebugService> logger, // inject ILogger
+            IConfiguration configuration  // inject IConfiguration
+            )
+        {
+            _logger = logger;
+            _configuration = configuration;
+        }
+
+        public void Start()
+        {
+            Console.WriteLine("Start debug...");
+
+            // debug code
+
+            Console.ReadLine();
+        }
+    }
+}
